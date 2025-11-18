c635a171965e0c69723bac184cf6d62c595732c4
Ruan Silva <progmruansilva@gmail.com>
2025-11-18 14:48:14 -0300

    ✨ (feat): Add header component

---
+++ assets/js/components/header.js
@@ -0,0 +1,25 @@
+class SHeader extends HTMLElement {
+  connectedCallback() {
+    this.classList.add('header');
+    this.innerHTML = `
+        <div class="header-left">
+          <div class="logo">Seito</div>
+          <nav class="nav">
+            <button class="nav-btn">🎓 Learning Plan</button>
+            <button class="nav-btn">👥</button>
+            <button class="nav-btn">❓</button>
+            <button class="nav-btn">✉️</button>
+            <button class="nav-btn">📅</button>
+          </nav>
+        </div>
+        <div class="user-info">
+          <div class="user-details">
+            <div class="user-name">Ellington Thom</div>
+            <div class="user-email">privatexx@mail.com</div>
+          </div>
+          <div class="user-avatar"></div>
+        </div>
+    `;
+  }
+}
+customElements.define("s-header", SHeader);
