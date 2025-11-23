8f34c2706e6ed903cddea6aacbcdad7e5497b8d5
harumiitakemoto <harumiitakemoto@gmail.com>
2025-11-23 10:41:16 -0300

    ✨ feat: add pag sobre

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -8,18 +8,13 @@ class SHeader extends HTMLElement {
     this.innerHTML = `
         <div class="header-left">
           <div class="logo">Seito</div>
-          <nav class="nav">
-            <a href="../index.html" class="nav-btn">🎓 Dashboard</a>
-            <button class="nav-btn">👥</button>
-            <button class="nav-btn">❓</button>
-            <button class="nav-btn">✉️</button>
-            <button class="nav-btn">📅</button>
-          </nav>
-        </div>
-        <div class="user-info">
-          <div class="user-details">
-            <div class="user-name">${storedUsername}</div>
-            <div class="user-email">${storedUserEmail}</div>
+            <nav class="nav">
+                <a href="../index.html" class="nav-btn">🎓</a>
+                <a href="../pages/integrantes.html" class="nav-btn">👥</a>
+                <a href="../pages/faq.html" class="nav-btn">❓</a>
+                <a href="../pages/sobre.html" class="nav-btn">✉️</a>
+                <button class="nav-btn">📅</button>
+            </nav>
           </div>
           <div class="user-info">
             <div class="user-details">
